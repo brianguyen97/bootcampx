24869f54f2448a576f4ecd4ac4d0bb52098440f4
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-06-10 02:24:46 -0600

    Refactor to prevent sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,35 +10,39 @@ const pool = new Pool({
 // pool
 //   .query(
 //     `
-// SELECT students.id as id, students.name as name, cohorts.name as cohort
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 // FROM students
-// JOIN cohorts ON cohort_id = cohorts.id
-// LIMIT 5;
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
 // `
 //   )
 //   .then((res) => {
 //     res.rows.forEach((user) => {
 //       console.log(
-//         `${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`
+//         `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
 //       );
 //     });
-//   });
+//   })
+//   .catch((err) => console.error("query error", err.stack));
 
-pool
-  .query(
-    `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
-  .then((res) => {
-    res.rows.forEach((user) => {
+const print = function () {
+  const args = process.argv.slice(2);
+  const queryString = `
+  SELECT students.id, students.name, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+  const values = [`${args[0]}%`, args[1]];
+  pool.query(queryString, values).then((res) => {
+    for (let row of res.rows) {
       console.log(
-        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+        `${row.name} has an id of ${row.id} and was in the ${row.cohort_name} cohort`
       );
-    });
-  })
-  .catch((err) => console.error("query error", err.stack));
+    }
+  });
+};
+
+print();
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,41 @@ const pool = new Pool({
   database: "postgres",
 });
 
-pool
-  .query(
-    `
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
-ORDER BY teacher;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((row) => {
+// pool
+//   .query(
+//     `
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+// ORDER BY teacher;
+// `
+//   )
+//   .then((res) => {
+//     res.rows.forEach((row) => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     });
+//   });
+
+const print = function () {
+  const args = process.argv.slice(2);
+  const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+  const values = [args[0]];
+  pool.query(queryString, values).then((res) => {
+    for (let row of res.rows) {
       console.log(`${row.cohort}: ${row.teacher}`);
-    });
+    }
   });
+};
+
+print();
